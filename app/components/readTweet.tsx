979740f07e1b93517f20ccf21fa7c04f55b7dca4
harshal-ishere <harshal011kamble@gmail.com>
2024-01-10 23:56:54 +0530

    just

--- app/components/readTweet.tsx
+++ app/components/readTweet.tsx
@@ -1,11 +1,13 @@
-import { FC } from "react";
-import { FaRegHeart } from "react-icons/fa6";
-import { FaRegComment } from "react-icons/fa";
+"use client"
+import { FC } from "react"
+import { FaRegComment } from "react-icons/fa"
+import { PiArrowFatDown,PiArrowFatUp } from "react-icons/pi"
 interface incProps{
     userId:String,
     post:String,
-    _id: String;
+    _id: String
 }
+
 let ReadTweet: FC<incProps> = ({userId,post}) => {
     return (
         <div className="readTweet px-44">
@@ -13,8 +15,12 @@ let ReadTweet: FC<incProps> = ({userId,post}) => {
                 <p>{userId}</p>
                 <p>{post}</p>
                 <div className="flex flex-row gap-16 mt-2">
+                    <div className="votes flex flex-row gap-3 items-center">
+                        <PiArrowFatUp size={"1.4rem"}  />
+                        <p>{}</p>
+                       <PiArrowFatDown size={"1.4rem"}   />
+                    </div>
                     <FaRegComment />
-                    <FaRegHeart />
                 </div>
             </div>
         </div>
