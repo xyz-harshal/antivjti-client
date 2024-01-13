2db7815a221a07580d2e66d5f05f8413d6dc8b1a
harshal-ishere <harshal011kamble@gmail.com>
2024-01-14 03:32:15 +0530

    did some images system

--- app/components/postTweet.tsx
+++ app/components/postTweet.tsx
@@ -4,21 +4,40 @@ import { usePostTweet } from "@/hooks/usePostTweet"
 import Cookies from "js-cookie";
 import { tweetPostDataType } from "@/types/types";
 import { counter } from "@/redux/features/reloadToggle";
+import { MdOutlineFileUpload } from "react-icons/md";
+import { useBase64 } from "@/hooks/useBase64";
+
 export default function PostTweet() {
-  let userId = Cookies.get('user')
-  let { tweet, textareaHeight,updateTextareaHeight,setTweet, handlePost } = usePostTweet("postTweets")
-  let [postData, setPostData] = useState<tweetPostDataType>();
+  let { convertToBase64 } = useBase64()
+  let { textareaHeight,updateTextareaHeight,handlePost} = usePostTweet("postTweets")
+  let [postData, setPostData] = useState<tweetPostDataType>({
+    userId:Cookies.get('user'),
+    post:"",
+    img:""
+  });
   const handleInputChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    setTweet(event.target.value);
     updateTextareaHeight(event.target);
     setPostData({
-        userId: userId,
-        post: event.target.value
-    });
-};
-let handleTweetPost=()=>{
-  handlePost(postData,counter)
-}
+      ...postData,
+      post:event.target.value
+    })
+  };
+  let handleFileUpload = async (e: any) => {
+    const file = e.target.files[0];
+    const base64 = await convertToBase64(file);
+    setPostData({
+      ...postData,
+      img:base64
+    })
+  }
+  let handleTweetPost = () => {
+    handlePost(postData, counter)
+    setPostData({
+      userId:Cookies.get('user'),
+      post:"",
+      img:""
+    })
+  }
   return (
     <div className="postTweet flex flex-col items-center px-44">
       <div className="w-full flex flex-row justify-center my-borderCol py-3">
@@ -26,17 +45,30 @@ let handleTweetPost=()=>{
       </div>
       <div className="flex flex-col items-center gap-4 my-borderCol px-8 py-4 w-full">
         <textarea
-          value={tweet}
+          value={postData.post}
           onChange={handleInputChange}
           style={{ height: textareaHeight }}
           className="bg-black outline-none resize-none w-full post-input my-6 overflow-y-hidden"
           placeholder="Tell us what happend today!"
         />
-        <div className="self-end ">
+        <div>
+          <img src={postData.img} alt="" />
+        </div>
+        <div className="flex flex-row justify-between w-full">
+          <label className="p-3 rounded-lg my-borderCol text-base outline-none hover:bg-white hover:text-black" htmlFor="uploadFile" >
+            <MdOutlineFileUpload size={'2rem'} />
+          </label>
           <button className="my-borderCol px-4 py-1 rounded-xl hover:bg-white hover:text-black" onClick={handleTweetPost}>Post</button>
         </div>
       </div>
-
+      <input
+        type="file"
+        name="myFile"
+        id="uploadFile"
+        accept='.jpeg, .png, .jpg'
+        onChange={(e) => handleFileUpload(e)}
+        className=" hidden"
+      />
     </div>
   )
 }
