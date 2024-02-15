3b9d25407e0f8cd25ce7beb216187f83f49f93c1
harshal-ishere <harshal011kamble@gmail.com>
2024-02-16 01:33:22 +0530

    customized home page

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 export default function Navbar() {
     let router = useRouter()
     return(
-        <div className="flex flex-row w-full justify-between items-center  py-2 px-8">
+        <div className="flex flex-row w-full justify-between items-center  py-2 px-8 border-b border-gray-500">
             <img src={logo.src} alt="heihe" className="w-36 cursor-pointer" onClick={()=>router.push('/')} />
             <FaGithub className="cursor-pointer" onClick={()=>router.push('https://github.com/xyz-harshal/antivjti-client')} size={'2rem'} />
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,39 @@
 "use client"
-import { useEffect, useState } from "react";
-import "./tailwind.css";
-import { useRouter } from "next/navigation";
-import Cookies from "js-cookie";
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { useAuth } from "@/hooks/useAuth"
+import Navbar from "./components/navbar"
+import { Inter,Lato } from "next/font/google"
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+})
+const lato = Lato({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+})
 export default function Home() {
-  let router = useRouter()
-  let cookie = Cookies?.get('user')
-  let [auth, setAuth] = useState<boolean>()
+  let router=useRouter()
+  let { authCheck } = useAuth()
   useEffect(() => {
-    if (cookie) setAuth(true)
-    else setAuth(false)
+    authCheck()
   }, [])
-  let data=[
-    {
-      data:'Unveil your thoughts freely on ANTI VJTI, the anonymous platform , exclusively for authentic and open conversations among vjtians.'
-    },
-    {
-      data:'Explore unfiltered opinions, ideas, and creativity from across departments and disciplines; all under one roof on Antivjti.'
-    },
-    {
-      data:'Stay updated on trending topics, events, and initiatives happening around your school through real-time updates and notifications on Antivjti.'
-    },
-    {
-      data:'Connect with friends and strangers alike over relatable memes and humorous anecdotes on Antivjti.'
-    }
-  ]
   return (
-    <div>
-      <section className="banner">
-        <div className="banner-content">
-          <h2>ANTI VJTI,AN ANONYMOUS FORUM</h2>
-          {!auth ? <div className="buttons">
-            <button className='LPbutton' onClick={() => router.push('/register')} >Register</button>
-            <button className='LPbutton' onClick={() => router.push('/login')}>Login</button>
-          </div> :
-            <div className="buttons">
-              <button className='LPbutton' onClick={() => router.push('/timeline')}>Join The Convo</button>
-            </div>}
+    <div className="flex flex-col">
+      <Navbar />
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-10 lg:gap-28 border-b border-gray-500 py-72">
+        <div className={lato.className+" flex flex-col items-center lg:items-start"}>
+          <h1 className="text-4xl font-bold">Welcome to Anti-VJTI</h1>
+          <p className="text-xl mt-4">A platform to post anything anonymously</p>
+          <p className="text-xl mt-2" >Only for the peps of VJTI</p>
         </div>
-      </section>
-      <div className="container">
-        {data.map((data,i)=>(
-          <div className="pan" key={i}>
-          <section className="panel">
-            {/* <img className="icon" src="" /> */}
-            <p className="F-content">
-              {data.data}
-            </p>
-          </section>
+        <div className={inter.className+" flex flex-col justify-center items-center py-4 gap-6"}>
+          <button className="bg-black text-white hover:bg-white my-borderCol hover:text-black px-4 py-2 rounded-md px-28 w-full text-xl" onClick={()=>router.push('/register')}>Register</button>
+          <button className="bg-black text-white hover:bg-white my-borderCol hover:text-black px-4 py-2 rounded-md px-28 w-full text-xl" onClick={()=>router.push('/login')}>Login</button>
         </div>
-        ))}
       </div>
-      
     </div>
   )
 }
