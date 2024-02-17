87e58d7181f1d0abcea971a1de53f8b3a02919cf
harshal-ishere <harshal011kamble@gmail.com>
2024-02-17 16:38:11 +0530

    minor commit

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     return(
         <div className="flex flex-row w-full justify-between items-center  py-2 px-8 border-b border-gray-500">
             <img src={logo.src} alt="logo" className="w-36 cursor-pointer" onClick={()=>router.push('/')} />
-            <a href="https://github.com/xyz-harshal/antivjti-client" target='_blank'><FaGithub className="cursor-pointer" size={'2rem'} /></a>
+            <a href="https://github.com/xyz-harshal/antivjti-client" target='_blank'><FaGithub className="cursor-pointer brag" size={'2rem'} /></a>
         </div>
     )
 }
--- app/data.ts
+++ app/data.ts
@@ -1,19 +1,14 @@
 import logo from '@/images/Anti-vjti.png'
+import post4 from '@/images/post4.jpeg'
 import post1 from '@/images/post1.jpeg'
 import post2 from '@/images/post2.jpeg'
 import post3 from '@/images/post3.png'
 import { homePageDataType } from '@/types/types'
 export let data:homePageDataType[]=[
     {
-        img:logo,
-        p1:'As the name says, it is a platform for the people who are against being normal who like to live on the edge',
-        p2:'It was build upon the concept of anonymity and security',
-        p3:'Anyone with an valid VJTI email Id can register here'
-    },
-    {
         img:post1,
         p1:'The purpose of this platform is to provide a place for the students to share their thoughts, ideas, and opinions anonymously',
-        p2:'To be honest the above statement is just bullshit, the real purpose is to have fun and talk about stuff that we cant talk in public',
+        p2:'To be honest the above statement is just bullshit, the real purpose is to have fun and talk about stuff that cant discussed in public',
         p3:'If you want to complain ,rant ,discuss ,share ,throw your opinions we got you covered'
     },
     {
@@ -21,6 +16,18 @@ export let data:homePageDataType[]=[
         p1:'This platform is not for the faint hearted, we talk about stuff that is not meant for the public eye',
         p2:'Buckle up and get ready to see the real side of VJTI, the side that is not shown to the public',
         p3:'We are not responsible for any of the content posted here, we are just a platform to post stuff'
+    },
+    {
+        img:logo,
+        p1:'As the name says, it is a platform for the people who are against being normal who like to live on the edge',
+        p2:'It was build upon the concept of anonymity and security',
+        p3:'Anyone with an valid VJTI email Id can register here'
+    },
+    {
+        img:post4,
+        p1:'Technically students can put memes, rants,discuss about the college, and anything related to college life anonymously',
+        p2:'No one will say that we copied the concept of reddit, but we did, and we are proud of it',
+        p3:'We don\'t want to compare our platform with any other, but we are better than the rest, and we know it'
     },
     {
         img:post3,
